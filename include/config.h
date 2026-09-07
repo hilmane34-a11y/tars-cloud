6d18bd0bd4e26a3db2c043096166de429be84f9b
hilmane34-a11y <hilmane34@gmail.com>
2026-09-07 14:39:40 +0700

    Create  config.h

---
+++ include/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define WIFI_SSID       "ISI_WIFI"
+#define WIFI_PASSWORD   "ISI_PASSWORD"
+
+#define BT_HEADSET_NAME "NAMA_HEADSET"
+
+#define OLED_SDA 21
+#define OLED_SCL 22
+
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+#define OLED_ADDR 0x3C
