527d860cce1be4a6dff399e052130ccbe2b0346d
hilmane34-a11y <hilmane34@gmail.com>
2026-09-15 12:00:24 +0700

    Update config.h

--- include/config.h
+++ include/config.h
@@ -1,30 +1,11 @@
 #pragma once
 
-// ============================================================
-// TARS CLOUD
-// ============================================================
+#define TARS_CLOUD_URL "https://tars-cloud-v1.hilmane34.workers.dev"
 
-#define TARS_CLOUD_URL  "https://tars-cloud-v1.hilmane34.workers.dev"
+#define OLED_SDA 21
+#define OLED_SCL 22
+#define OLED_ADDR 0x3C
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
 
-// ============================================================
-// OLED SSD1306
-// ============================================================
-
-#define OLED_SDA        21
-#define OLED_SCL        22
-#define OLED_ADDR       0x3C
-
-#define OLED_WIDTH      128
-#define OLED_HEIGHT     64
-
-// ============================================================
-// MP3
-// ============================================================
-
-#define MP3_FILE        "/tts.mp3"
-
-// ============================================================
-// TARS
-// ============================================================
-
-#define SERIAL_BAUD     115200
+#define SERIAL_BAUD 115200
